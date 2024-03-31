df8efb26fbc43cdfb00633a36a3627158459fb58
JadeKim042386 <jk042386@gmail.com>
2024-03-31 20:54:29 +0900

    spring project setup

---
+++ src/main/java/freshtrash/freshtrashbackend/FreshTrashBackendApplication.java
@@ -0,0 +1,13 @@
+package freshtrash.freshtrashbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FreshTrashBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FreshTrashBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/freshtrash/freshtrashbackend/FreshTrashBackendApplicationTests.java
@@ -0,0 +1,15 @@
+package freshtrash.freshtrashbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@ActiveProfiles("test")
+@SpringBootTest
+class FreshTrashBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
