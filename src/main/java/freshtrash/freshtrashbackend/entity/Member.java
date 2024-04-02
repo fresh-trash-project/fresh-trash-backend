502b2ecffcd7de6f5904f3dc3b7ef173bbfa4b8e
Soyoon <spurt@tukorea.ac.kr>
2024-04-02 16:55:20 +0900

    refactor: #2 - 외부에서 기본 생성자에 접근을 제한

--- src/main/java/freshtrash/freshtrashbackend/entity/Member.java
+++ src/main/java/freshtrash/freshtrashbackend/entity/Member.java
@@ -11,7 +11,7 @@ import javax.persistence.*;
 import java.time.LocalDateTime;
 
 @Entity
-@NoArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Table(name="members")
 @Getter
 @Setter
