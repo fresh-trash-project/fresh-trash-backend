e94c44bc3e12293ccf120db5daede927f742ae29
Soyoon <spurt@tukorea.ac.kr>
2024-04-03 01:46:29 +0900

    chore: #2 - DB에 members 테이블 생성 후 데이터 추가

--- database/initdb.d/create_table.sql
+++ database/initdb.d/create_table.sql
@@ -1,17 +1,34 @@
-﻿CREATE TABLE `wastes`
+﻿CREATE TABLE `members`
 (
     `id`             bigint AUTO_INCREMENT NOT NULL,
-    `title`          varchar(255)          NOT NULL,
-    `content`        text                  NOT NULL,
-    `waste_price`     integer               NOT NULL,
-    `like_count`      integer               NOT NULL,
-    `view_count`      integer               NOT NULL,
-    `file_name`       varchar(255)          NULL,
-    `waste_category`  varchar(255)          NOT NULL,
-    `waste_status`    varchar(255)          NOT NULL,
-    `sell_status`     varchar(255)          NOT NULL,
-    `address`        json                  NOT NULL,
-    `created_at`     datetime              NOT NULL,
+    `email`          varchar(255) NOT NULL,
+    `rating`         double       NOT NULL,
+    `nickname`       varchar(255) NOT NULL,
+    `address`        json,
+    `file_name`      varchar(255),
+    `login_type`     varchar(255) NOT NULL,
+    `user_role`      varchar(255) NOT NULL,
+    `account_status` varchar(255) NOT NULL,
+    `created_at`     datetime     NOT NULL,
+    `modified_at`    datetime,
+    PRIMARY KEY (`id`)
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8mb4 COMMENT ='멤버';
+
+CREATE TABLE `wastes`
+(
+    `id`             bigint AUTO_INCREMENT NOT NULL,
+    `title`          varchar(255) NOT NULL,
+    `content`        text         NOT NULL,
+    `waste_price`    integer      NOT NULL,
+    `like_count`     integer      NOT NULL,
+    `view_count`     integer      NOT NULL,
+    `file_name`      varchar(255),
+    `waste_category` varchar(255) NOT NULL,
+    `waste_status`   varchar(255) NOT NULL,
+    `sell_status`    varchar(255) NOT NULL,
+    `address`        json         NOT NULL,
+    `created_at`     datetime     NOT NULL,
     `modified_at`    datetime,
     `transaction_at` datetime,
     PRIMARY KEY (`id`)
