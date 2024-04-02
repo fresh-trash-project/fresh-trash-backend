67d5ec81c2c0cfc35ad495900b05e249e72880d9
Soyoon <spurt@tukorea.ac.kr>
2024-04-02 17:00:12 +0900

    chore: #2 - ID 생성 전략을 AUTO에서 IDENTITY로 변경

--- src/main/java/freshtrash/freshtrashbackend/entity/Member.java
+++ src/main/java/freshtrash/freshtrashbackend/entity/Member.java
@@ -18,7 +18,7 @@ import java.time.LocalDateTime;
 @ToString
 public class Member {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id")
     private Long id;
 
