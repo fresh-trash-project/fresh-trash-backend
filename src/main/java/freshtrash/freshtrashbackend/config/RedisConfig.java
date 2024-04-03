2bce4e49d2d4db4a2a0850dd7604d777b17d280b
garamv20 <garamv20@gmail.com>
2024-04-03 19:49:57 +0900

    chore: #3-불필요 import 삭제

--- src/main/java/freshtrash/freshtrashbackend/config/RedisConfig.java
+++ src/main/java/freshtrash/freshtrashbackend/config/RedisConfig.java
@@ -2,7 +2,6 @@ package freshtrash.freshtrashbackend.config;
 
 import freshtrash.freshtrashbackend.dto.properties.RedisInfoProperties;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cache.CacheManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.redis.cache.RedisCacheConfiguration;
@@ -11,7 +10,6 @@ import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.RedisSerializationContext;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
