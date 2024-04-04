1208e019cf83c10a0bb1e8410bfd4780e8d44de8
Soyoon <spurt@tukorea.ac.kr>
2024-04-05 00:04:20 +0900

    feat: #1 - SecurityFilterChain 빈을 직접 정의

---
+++ src/main/java/freshtrash/freshtrashbackend/config/SecurityConfig.java
@@ -0,0 +1,24 @@
+package freshtrash.freshtrashbackend.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests(authorize -> authorize
+                        .antMatchers("/public/**").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .formLogin(withDefaults());
+        return http.build();
+    }
+}
+
