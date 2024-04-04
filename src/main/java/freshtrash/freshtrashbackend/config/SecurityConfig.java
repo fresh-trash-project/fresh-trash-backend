09fc1b2240f67e080a30a1a630925ef4bb0ecbc1
Soyoon <spurt@tukorea.ac.kr>
2024-04-05 00:42:21 +0900

    feat: #1 - 패스워드 인코딩을 위한 PasswordEncoder 빈 등록

--- src/main/java/freshtrash/freshtrashbackend/config/SecurityConfig.java
+++ src/main/java/freshtrash/freshtrashbackend/config/SecurityConfig.java
@@ -1,12 +1,16 @@
 package freshtrash.freshtrashbackend.config;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
+@Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 
@@ -19,6 +23,11 @@ public class SecurityConfig {
                 )
                 .formLogin(withDefaults());
         return http.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
 
