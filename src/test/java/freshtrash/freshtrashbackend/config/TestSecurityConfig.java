9a3b8f89c342beac0738a15279f32445ee3be616
JadeKim042386 <jk042386@gmail.com>
2024-04-06 02:20:12 +0900

    test: #26 - 유저 정보 추가에 따른 테스트 코드 수정

--- src/test/java/freshtrash/freshtrashbackend/config/TestSecurityConfig.java
+++ src/test/java/freshtrash/freshtrashbackend/config/TestSecurityConfig.java
@@ -1,11 +1,30 @@
 package freshtrash.freshtrashbackend.config;
 
+import freshtrash.freshtrashbackend.Fixture.Fixture;
+import freshtrash.freshtrashbackend.entity.Member;
+import freshtrash.freshtrashbackend.entity.constants.AccountStatus;
+import freshtrash.freshtrashbackend.entity.constants.LoginType;
+import freshtrash.freshtrashbackend.entity.constants.UserRole;
 import freshtrash.freshtrashbackend.security.SecurityConfig;
 import freshtrash.freshtrashbackend.service.MemberService;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Import;
+import org.springframework.test.context.event.annotation.BeforeTestMethod;
+
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.BDDMockito.given;
 
 @Import(SecurityConfig.class)
 public class TestSecurityConfig {
     @MockBean private MemberService memberService;
+
+    @BeforeTestMethod
+    void securitySetUp() {
+        String userEmail = "testUser@gmail.com";
+        given(memberService.getMemberEntityByEmail(eq(userEmail))).willReturn(createMember(userEmail));
+    }
+
+    private Member createMember(String email) {
+        return Fixture.createMember(email, "pw", "test", LoginType.EMAIL, UserRole.USER, AccountStatus.ACTIVE);
+    }
 }
