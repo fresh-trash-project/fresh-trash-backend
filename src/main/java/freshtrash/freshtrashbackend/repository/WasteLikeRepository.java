55455f9dffe2216cd6a11317747bd60866375a57
garamv20 <garamv20@gmail.com>
2024-04-07 02:03:00 +0900

    feat: #30-WasteLike entity, repository 정의

---
+++ src/main/java/freshtrash/freshtrashbackend/repository/WasteLikeRepository.java
@@ -0,0 +1,6 @@
+package freshtrash.freshtrashbackend.repository;
+
+import freshtrash.freshtrashbackend.entity.WasteLike;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface WasteLikeRepository extends JpaRepository<WasteLike, Long> {}
