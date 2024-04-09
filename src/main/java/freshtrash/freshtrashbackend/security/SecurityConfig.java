dc812354a9e9cad886390e7e982c88047935b666
JadeKim042386 <jk042386@gmail.com>
2024-04-09 19:01:50 +0900

    feat: #39 - OAuth 로그인을 위한 SecurityConfig 설정 - OAuth2UserService 구현 및 빈 등록 - OAuth 로그인 시 회원가입 되지 않은 유저라면 UserRole을 ANONYMOUS로 지정하여 반환 (id도 null 값을 가짐) - OAuth 로그인 성공 후 successHandler를 통해 accessToken을 발급하여 response

--- src/main/java/freshtrash/freshtrashbackend/security/SecurityConfig.java
+++ src/main/java/freshtrash/freshtrashbackend/security/SecurityConfig.java
@@ -1,6 +1,9 @@
 package freshtrash.freshtrashbackend.security;
 
 import freshtrash.freshtrashbackend.dto.security.MemberPrincipal;
+import freshtrash.freshtrashbackend.dto.security.OAuthAttributes;
+import freshtrash.freshtrashbackend.entity.constants.UserRole;
+import freshtrash.freshtrashbackend.exception.MemberException;
 import freshtrash.freshtrashbackend.service.MemberService;
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
@@ -9,15 +12,27 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserService;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.access.ExceptionTranslationFilter;
-import org.springframework.security.web.session.SessionManagementFilter;
+
+import java.util.UUID;
 
 @Configuration
 public class SecurityConfig {
 
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http, JwtTokenFilter jwtTokenFilter, Http401UnauthorizedAuthenticationEntryPoint authenticationEntryPoint) throws Exception {
+    public SecurityFilterChain securityFilterChain(
+            HttpSecurity http,
+            OAuth2UserService<OAuth2UserRequest, OAuth2User> oAuth2UserService,
+            JwtTokenFilter jwtTokenFilter,
+            Http401UnauthorizedAuthenticationEntryPoint authenticationEntryPoint,
+            CustomOAuth2SuccessHandler customOAuth2SuccessHandler)
+            throws Exception {
         return http.csrf()
                 .disable()
                 .authorizeHttpRequests(auth -> auth.requestMatchers(
@@ -28,11 +43,45 @@ public class SecurityConfig {
                 .sessionManagement()
                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and()
-                    .addFilterAfter(jwtTokenFilter, ExceptionTranslationFilter.class)
-                    .exceptionHandling()
-                    .authenticationEntryPoint(authenticationEntryPoint)
+                .oauth2Login(oAuth -> oAuth.userInfoEndpoint(userInfo -> userInfo.userService(oAuth2UserService))
+                        .successHandler(customOAuth2SuccessHandler))
+                .addFilterAfter(jwtTokenFilter, ExceptionTranslationFilter.class)
+                .exceptionHandling()
+                .authenticationEntryPoint(authenticationEntryPoint)
                 .and()
-                    .build();
+                .build();
+    }
+
+    @Bean
+    public OAuth2UserService<OAuth2UserRequest, OAuth2User> oAuth2UserService(
+            MemberService memberService, PasswordEncoder passwordEncoder) {
+        final DefaultOAuth2UserService delegate = new DefaultOAuth2UserService();
+
+        return userRequest -> {
+            OAuth2User oAuth2User = delegate.loadUser(userRequest);
+            String registrationId = userRequest.getClientRegistration().getRegistrationId();
+            String userNameAttributeName = userRequest
+                    .getClientRegistration()
+                    .getProviderDetails()
+                    .getUserInfoEndpoint()
+                    .getUserNameAttributeName();
+            OAuthAttributes attributes =
+                    OAuthAttributes.of(registrationId, userNameAttributeName, oAuth2User.getAttributes());
+            String email = attributes.getEmail();
+            String dummyPassword = passwordEncoder.encode("{bcrypt}" + UUID.randomUUID());
+
+            // 회원이 존재하지 않는다면 저장하지 않고 회원가입이 안된 상태로 반환
+            try {
+                return MemberPrincipal.fromEntity(memberService.getMemberEntityByEmail(email));
+            } catch (MemberException e) {
+                return MemberPrincipal.builder()
+                        .email(email)
+                        .password(dummyPassword)
+                        .nickname(attributes.getName())
+                        .authorities(UserRole.ANONYMOUS)
+                        .build();
+            }
+        };
     }
 
     @Bean
