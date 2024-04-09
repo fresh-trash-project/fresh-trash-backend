4424b402abb516fc80527c02b10c1cbfdbf651ef
JadeKim042386 <jk042386@gmail.com>
2024-04-09 12:09:37 +0900

    test: #29 - Application Test 삭제

--- src/test/java/freshtrash/freshtrashbackend/controller/WasteApiTest.java
+++ src/test/java/freshtrash/freshtrashbackend/controller/WasteApiTest.java
@@ -24,6 +24,7 @@ import org.springframework.http.HttpMethod;
 import org.springframework.http.MediaType;
 import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.security.test.context.support.WithUserDetails;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.web.multipart.MultipartFile;
 
