fd0a80b063275f04f9c7b61633ccd1ea180e383c
garamv20 <garamv20@gmail.com>
2024-04-09 14:19:20 +0900

    feat: #31-updateLikeCount 파라미터 표시 변경

--- src/main/java/freshtrash/freshtrashbackend/repository/WasteRepository.java
+++ src/main/java/freshtrash/freshtrashbackend/repository/WasteRepository.java
@@ -1,11 +1,11 @@
 package freshtrash.freshtrashbackend.repository;
 
 import freshtrash.freshtrashbackend.entity.Waste;
+
 import freshtrash.freshtrashbackend.repository.projections.FileNameSummary;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.EntityGraph;
-import io.lettuce.core.dynamic.annotation.Param;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
@@ -25,6 +25,6 @@ public interface WasteRepository extends JpaRepository<Waste, Long> {
     boolean existsByIdAndMember_Id(Long wasteId, Long memberId);
 
     @Modifying(clearAutomatically = true, flushAutomatically = true)
-    @Query("update Waste w set w.likeCount = w.likeCount + :updateCount where w.id = :wasteId")
-    int updateLikeCount(@Param("wasteId") Long wasteId, @Param("updateCount") int updateCount);
+    @Query("update Waste w set w.likeCount = w.likeCount + ?2 where w.id = ?1")
+    int updateLikeCount(Long wasteId, int updateCount);
 }
