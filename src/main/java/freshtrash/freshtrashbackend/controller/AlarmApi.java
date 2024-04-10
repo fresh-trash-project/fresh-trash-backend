2b86dce3cf6e28edf0a9c8a411b5e3ca1349ffbd
JadeKim042386 <jk042386@gmail.com>
2024-04-10 23:24:39 +0900

    feat: #47 - 알람 전체 조회 API 구현 - 임의로 페이징 처리 적용 - 로그인한 유저의 Id와 읽지 않은 알람만 조회

--- src/main/java/freshtrash/freshtrashbackend/controller/AlarmApi.java
+++ src/main/java/freshtrash/freshtrashbackend/controller/AlarmApi.java
@@ -1,8 +1,12 @@
 package freshtrash.freshtrashbackend.controller;
 
+import freshtrash.freshtrashbackend.dto.AlarmDto;
 import freshtrash.freshtrashbackend.dto.security.MemberPrincipal;
 import freshtrash.freshtrashbackend.service.AlarmService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,6 +19,16 @@ import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 @RequiredArgsConstructor
 public class AlarmApi {
     private final AlarmService alarmService;
+
+    /**
+     * 현재 로그인한 사용자에게 온 알람 조회
+     */
+    // TODO: 이후 프론트와 협의를 통해 페이징 처리가 아닌 리스트로 반환되도록 수정될 수 있습니다
+    @GetMapping
+    public ResponseEntity<Page<AlarmDto>> getAlarms(
+            @PageableDefault Pageable pageable, @AuthenticationPrincipal MemberPrincipal memberPrincipal) {
+        return ResponseEntity.ok(alarmService.getAlarms(memberPrincipal.id(), pageable));
+    }
 
     /**
      * SSE 연결 요청
--- src/main/java/freshtrash/freshtrashbackend/repository/AlarmRepository.java
+++ src/main/java/freshtrash/freshtrashbackend/repository/AlarmRepository.java
@@ -1,6 +1,10 @@
 package freshtrash.freshtrashbackend.repository;
 
 import freshtrash.freshtrashbackend.entity.Alarm;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface AlarmRepository extends JpaRepository<Alarm, Long> {}
+public interface AlarmRepository extends JpaRepository<Alarm, Long> {
+    Page<Alarm> findAllByMember_IdAndReadAtIsNull(Long memberId, Pageable pageable);
+}
