da28888ff7a0fa08913d9dcea4bbb5695d1b0de3
garamv20 <garamv20@gmail.com>
2024-04-10 18:51:38 +0900

    refactor: #41-existsByWasteIdAndMemberId 삭제  - existsByWasteId 추가

--- src/main/java/freshtrash/freshtrashbackend/repository/WasteReviewRepository.java
+++ src/main/java/freshtrash/freshtrashbackend/repository/WasteReviewRepository.java
@@ -4,5 +4,5 @@ import freshtrash.freshtrashbackend.entity.WasteReview;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface WasteReviewRepository extends JpaRepository<WasteReview, Long> {
-    boolean existsByWasteIdAndMemberId(Long wasteId, Long memberId);
+    boolean existsByWasteId(Long wasteId);
 }
