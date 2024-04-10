72fb1d870d73d7805f0adf8a1b4316ff9227a11c
JadeKim042386 <jk042386@gmail.com>
2024-04-10 22:40:24 +0900

    feat: #46 - SseEmitter onError 이벤트 처리 추가 - 에러 발생 시 SseEmitter 삭제

--- src/main/java/freshtrash/freshtrashbackend/service/AlarmService.java
+++ src/main/java/freshtrash/freshtrashbackend/service/AlarmService.java
@@ -5,12 +5,14 @@ import freshtrash.freshtrashbackend.exception.constants.ErrorCode;
 import freshtrash.freshtrashbackend.repository.AlarmRepository;
 import freshtrash.freshtrashbackend.repository.EmitterRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
 import java.io.IOException;
 import java.util.concurrent.TimeUnit;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class AlarmService {
@@ -23,6 +25,10 @@ public class AlarmService {
         emitterRepository.save(memberId, sseEmitter);
         sseEmitter.onCompletion(() -> emitterRepository.delete(memberId));
         sseEmitter.onTimeout(() -> emitterRepository.delete(memberId));
+        sseEmitter.onError((e) -> {
+            emitterRepository.delete(memberId);
+            log.error("SseEmitter Error: ", e);
+        });
 
         try {
             // 처음 SSE 연결 후 아무런 이벤트도 보내지 않으면 재연결 요청을 보낼때나 연결 요청 자체에서 에러가 발생합니다. 따라서 임의로 데이터를 전송합니다.
