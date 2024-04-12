6f2346acedbefd41bb5b0fbcb6571667cb29231b
Soyoon <spurt@tukorea.ac.kr>
2024-04-12 19:51:23 +0900

    refactor: 44# - rating 초기값 설정 로직 제거, 자동으로 0으로 초기화됨
    현재 rating 필드의 지정된 type의 경우 primitive type이기 때문에 (따로 설정하지 않아도 0으로 초기화되기 때문에) rating 초기값 설정 로직을 제거하여 필요 없는 초기화 로직을 줄였습니다.

--- src/main/java/freshtrash/freshtrashbackend/entity/Member.java
+++ src/main/java/freshtrash/freshtrashbackend/entity/Member.java
@@ -66,11 +66,6 @@ public class Member extends AuditingAt implements Persistable<Long> {
     @OneToMany(mappedBy = "member", cascade = ALL, fetch = LAZY)
     private Set<Waste> wastes = new LinkedHashSet<>();
 
-    @PrePersist
-    private void prePersist() {
-        this.rating = 0;
-    }
-
     @Builder // 빌더 패턴 적용
     public Member(String email, String password, String nickname, Address address, String fileName, LoginType loginType, UserRole userRole, AccountStatus accountStatus) {
         this.email = email;
