ad5f0778ed6b6af224374d010c68582ecc8afcf3
Soyoon <spurt@tukorea.ac.kr>
2024-04-14 03:14:59 +0900

    refactor: #61 - @Repository 어노테이션 제거
    스프링 데이터 JPA가 레포지토리 인터페이스에 대한 자동 구현 및 예외 변환을 제공하기 때문에, @Repository 어노테이션을 명시적으로 붙일 필요가 없어 간결함과 명확성을 위해 제거

--- src/main/java/freshtrash/freshtrashbackend/repository/ChatRoomRepository.java
+++ src/main/java/freshtrash/freshtrashbackend/repository/ChatRoomRepository.java
@@ -4,5 +4,4 @@ import freshtrash.freshtrashbackend.entity.ChatRoom;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-@Repository
 public interface ChatRoomRepository extends JpaRepository<ChatRoom, Long> {}
