6fc06b690d5fd4cd08c6167a281cfe76d1ccc92d
JadeKim042386 <jk042386@gmail.com>
2024-04-15 16:57:54 +0900

    feat: #59 - RabbitMQ Config 설정 - waste 거래 알람 메시지를 저장할 queue 추가 - directExchange 빈 등록

---
+++ src/main/java/freshtrash/freshtrashbackend/config/RabbitMQConfig.java
@@ -0,0 +1,33 @@
+package freshtrash.freshtrashbackend.config;
+
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.DirectExchange;
+import org.springframework.amqp.core.Queue;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RabbitMQConfig {
+    public static final String WASTE_ID_KEY = "wasteId";
+    public static final String MEMBER_ID_KEY = "memberId";
+    public static final String WASTE_TRANSACTION_ROUTING_KEY = "waste.transaction";
+    private static final String directExchangeName = "waste-direct-exchange";
+
+    private static final String WASTE_QUEUE = "waste-queue";
+
+    @Bean
+    Queue wasteQueue() {
+        return new Queue(WASTE_QUEUE, false);
+    }
+
+    @Bean
+    DirectExchange directExchange() {
+        return new DirectExchange(directExchangeName);
+    }
+
+    @Bean
+    Binding directBinding(Queue queue, DirectExchange directExchange) {
+        return BindingBuilder.bind(queue).to(directExchange).with(WASTE_TRANSACTION_ROUTING_KEY);
+    }
+}
