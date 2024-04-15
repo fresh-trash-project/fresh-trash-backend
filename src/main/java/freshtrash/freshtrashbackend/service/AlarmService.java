ecef9d9fcdb52de58b22e25a31cd8c1c30d228e8
JadeKim042386 <jk042386@gmail.com>
2024-04-15 17:02:08 +0900

    feat: #59 - waste 거래 완료 후 알람 전송 API 구현 - 대상 waste의 채팅방들을 모두 조회하고 판매자, 구매자, 구매자가 아닌 사용자들에게 알람 전송 - 거래 내역 저장

--- src/main/java/freshtrash/freshtrashbackend/service/AlarmService.java
+++ src/main/java/freshtrash/freshtrashbackend/service/AlarmService.java
@@ -1,12 +1,19 @@
 package freshtrash.freshtrashbackend.service;
 
-import freshtrash.freshtrashbackend.dto.AlarmDto;
+import freshtrash.freshtrashbackend.config.RabbitMQConfig;
+import freshtrash.freshtrashbackend.dto.response.AlarmResponse;
+import freshtrash.freshtrashbackend.entity.Alarm;
+import freshtrash.freshtrashbackend.entity.AlarmArgs;
+import freshtrash.freshtrashbackend.entity.constants.AlarmType;
 import freshtrash.freshtrashbackend.exception.AlarmException;
 import freshtrash.freshtrashbackend.exception.constants.ErrorCode;
 import freshtrash.freshtrashbackend.repository.AlarmRepository;
 import freshtrash.freshtrashbackend.repository.EmitterRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.amqp.core.Message;
+import org.springframework.amqp.core.MessageProperties;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
@@ -15,11 +22,15 @@ import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 import java.io.IOException;
 import java.util.concurrent.TimeUnit;
 
+import static java.nio.charset.StandardCharsets.UTF_8;
+
 @Slf4j
 @Service
 @RequiredArgsConstructor
 public class AlarmService {
     private static final Long SSE_TIMEOUT = TimeUnit.MINUTES.toMillis(30);
+    private static final String CONNECTED_ALARM_NAME = "connected";
+    private static final String WASTE_TRANSACTION_ALARM_NAME = "waste-transaction-alarm";
     private final EmitterRepository emitterRepository;
     private final AlarmRepository alarmRepository;
 
@@ -27,8 +38,60 @@ public class AlarmService {
      * 전체 알람 조회
      * - 읽지 않은 알람(readAt == null)만 조회
      */
-    public Page<AlarmDto> getAlarms(Long memberId, Pageable pageable) {
-        return alarmRepository.findAllByMember_IdAndReadAtIsNull(memberId, pageable).map(AlarmDto::fromEntity);
+    public Page<AlarmResponse> getAlarms(Long memberId, Pageable pageable) {
+        return alarmRepository
+                .findAllByMember_IdAndReadAtIsNull(memberId, pageable)
+                .map(Alarm::toResponse);
+    }
+
+    /**
+     * 알람 저장
+     * - 폐기물 거래 완료 알람
+     * @param memberId 알람을 받는 사용자 id
+     * @param targetId 폐기물 id
+     */
+    public Alarm saveAlarm(String message, Long memberId, Long targetId) {
+        return alarmRepository.save(Alarm.builder()
+                .alarmType(AlarmType.TRANSACTION)
+                .alarmArgs(AlarmArgs.of(targetId))
+                .message(message)
+                .memberId(memberId)
+                .build());
+    }
+
+    /**
+     * 거래 완료 메시지 전송 Listener
+     */
+    @RabbitListener(queues = "#{wasteQueue.name}")
+    public void receiveWasteTransaction(Message message) {
+        MessageProperties messageProperties = message.getMessageProperties();
+        receive(
+                new String(message.getBody(), UTF_8),
+                messageProperties.getHeader(RabbitMQConfig.MEMBER_ID_KEY),
+                messageProperties.getHeader(RabbitMQConfig.WASTE_ID_KEY));
+    }
+
+    /**
+     * SSE 알람 전송
+     * @param memberId 알람을 받는 사용자 id
+     */
+    private void receive(String message, Long memberId, Long targetId) {
+        Alarm alarm = saveAlarm(message, memberId, targetId);
+        emitterRepository
+                .get(memberId)
+                .ifPresentOrElse(
+                        sseEmitter -> {
+                            try {
+                                sseEmitter.send(SseEmitter.event()
+                                        .id(String.valueOf(alarm.getId()))
+                                        .name(WASTE_TRANSACTION_ALARM_NAME)
+                                        .data(alarm.toResponse()));
+                            } catch (IOException e) {
+                                emitterRepository.delete(memberId);
+                                throw new AlarmException(ErrorCode.ALARM_CONNECT_ERROR, e);
+                            }
+                        },
+                        () -> log.error("Emiter를 찾을 수 없습니다."));
     }
 
     /**
@@ -46,7 +109,7 @@ public class AlarmService {
 
         try {
             // 처음 SSE 연결 후 아무런 이벤트도 보내지 않으면 재연결 요청을 보낼때나 연결 요청 자체에서 에러가 발생합니다. 따라서 임의로 데이터를 전송합니다.
-            sseEmitter.send(SseEmitter.event().id("").name("connect").data("connect completed"));
+            sseEmitter.send(SseEmitter.event().id("").name(CONNECTED_ALARM_NAME).data("connect completed"));
         } catch (IOException e) {
             throw new AlarmException(ErrorCode.ALARM_CONNECT_ERROR, e);
         }
