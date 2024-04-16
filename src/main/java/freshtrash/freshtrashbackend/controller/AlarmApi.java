b7912f619e4bd3cd31d7b6603a2fe82563abc5cb
JadeKim042386 <jk042386@gmail.com>
2024-04-16 19:50:11 +0900

    feat: #71 - 알람 읽음 처리 요청 API 구현 - 알람 읽음 처리 요청 시 로그인한 사용자가 대상 알람의 주인인지 확인 - native query로 알람의 readAt을 읽은 일시로 update

--- src/main/java/freshtrash/freshtrashbackend/controller/AlarmApi.java
+++ src/main/java/freshtrash/freshtrashbackend/controller/AlarmApi.java
@@ -2,6 +2,8 @@ package freshtrash.freshtrashbackend.controller;
 
 import freshtrash.freshtrashbackend.dto.response.AlarmResponse;
 import freshtrash.freshtrashbackend.dto.security.MemberPrincipal;
+import freshtrash.freshtrashbackend.exception.AlarmException;
+import freshtrash.freshtrashbackend.exception.constants.ErrorCode;
 import freshtrash.freshtrashbackend.service.AlarmService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
@@ -10,6 +12,7 @@ import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
@@ -36,5 +39,23 @@ public class AlarmApi {
     @GetMapping("/subscribe")
     public ResponseEntity<SseEmitter> subscribe(@AuthenticationPrincipal MemberPrincipal memberPrincipal) {
         return ResponseEntity.ok(alarmService.connectAlarm(memberPrincipal.id()));
+    }
+
+    /**
+     * 알람 읽음 처리 요청
+     */
+    @GetMapping("/{alarmId}")
+    public ResponseEntity<Void> readAlarm(
+            @PathVariable Long alarmId, @AuthenticationPrincipal MemberPrincipal memberPrincipal) {
+        checkIfOwnerOfAlarm(alarmId, memberPrincipal.id());
+        alarmService.readAlarm(alarmId);
+        return ResponseEntity.ok(null);
+    }
+
+    /**
+     * 로그인한 사용자가 대상 알람의 주인인지 확인
+     */
+    private void checkIfOwnerOfAlarm(Long alarmId, Long memberId) {
+        if (!alarmService.isOwnerOfAlarm(alarmId, memberId)) throw new AlarmException(ErrorCode.FORBIDDEN_ALARM);
     }
 }
--- src/main/java/freshtrash/freshtrashbackend/repository/AlarmRepository.java
+++ src/main/java/freshtrash/freshtrashbackend/repository/AlarmRepository.java
@@ -4,7 +4,15 @@ import freshtrash.freshtrashbackend.entity.Alarm;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 
 public interface AlarmRepository extends JpaRepository<Alarm, Long> {
     Page<Alarm> findAllByMember_IdAndReadAtIsNull(Long memberId, Pageable pageable);
+
+    @Query(nativeQuery = true, value = "update alarms a set a.read_at = now() where a.id = ?1")
+    void updateReadAtById(Long alarmId);
+
+
+    boolean existsByIdAndMember_Id(Long alarmId, Long memberId);
 }
