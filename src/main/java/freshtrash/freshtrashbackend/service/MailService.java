12683e67f739975b759430d9ac16f762ccb3071d
garamv20 <garamv20@gmail.com>
2024-04-16 17:13:21 +0900

    feat: #60-메일 유효성 검증 기능 구현 - 메일 검증 에러코드 추가

--- src/main/java/freshtrash/freshtrashbackend/service/MailService.java
+++ src/main/java/freshtrash/freshtrashbackend/service/MailService.java
@@ -1,5 +1,8 @@
 package freshtrash.freshtrashbackend.service;
 
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import freshtrash.freshtrashbackend.dto.properties.MailProperties;
 import freshtrash.freshtrashbackend.exception.MailException;
 import freshtrash.freshtrashbackend.exception.constants.ErrorCode;
 import lombok.RequiredArgsConstructor;
@@ -10,6 +13,11 @@ import org.springframework.stereotype.Service;
 import org.springframework.util.StringUtils;
 
 import javax.mail.internet.MimeMessage;
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
 
 @Slf4j
 @Service
@@ -17,11 +25,13 @@ import javax.mail.internet.MimeMessage;
 public class MailService {
     private final JavaMailSender mailSender;
     private final RedisService redisService;
+    private final MailProperties mailProperties;
     private final int AUTH_CODE_DURATION_MIN = 10;
     private final int AUTH_SUCCESS_DURATION_MIN = 300;
     private final String AUTH_SUCCESS = "AuthSuccess";
 
     public void sendMailWithCode(String email, String subject, String code) {
+        isValidMail(email);
         String text = "fresh-trash 메일 인증 코드입니다. <br/>인증코드:" + code;
         sendMail(email, subject, text);
         redisService.saveEmailVerificationCode(email, code, AUTH_CODE_DURATION_MIN);
@@ -54,6 +64,37 @@ public class MailService {
             mailSender.send(mimeMessage);
         } catch (Exception e) {
             throw new MailException(ErrorCode.MAIL_SEND_FAIL, e);
+        }
+    }
+
+    /**
+     * 메일 유효성 검증
+     */
+    private void isValidMail(String email) {
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create("https://emailvalidation.abstractapi.com/v1/?api_key=" + mailProperties.apiKey()
+                        + "&email=" + email))
+                .build();
+
+        try {
+            HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+            String result = response.body();
+            JsonObject jsonObject = JsonParser.parseString(result).getAsJsonObject();
+            String deliverable = jsonObject.get("deliverability").getAsString();
+            System.out.println(deliverable);
+            boolean isFree = jsonObject
+                    .get("is_free_email")
+                    .getAsJsonObject()
+                    .get("value")
+                    .getAsBoolean();
+
+            if (deliverable.equals("UNDELIVERABLE") || !isFree) {
+                throw new MailException(ErrorCode.MAIL_NOT_VALID);
+            }
+
+        } catch (IOException | InterruptedException e) {
+            throw new MailException(ErrorCode.MAIL_VALIDATION_FAIL, e);
         }
     }
 }
