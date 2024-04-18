e3b37c97afc0c36cc4824059e752af1b5ca9f08a
garamv20 <garamv20@gmail.com>
2024-04-19 01:28:12 +0900

    refactor: #81-to를 from메소드로 통일 - SignUpRequest의 toEntity -> fromSignUpRequest 로 수정

--- src/main/java/freshtrash/freshtrashbackend/dto/request/SignUpRequest.java
+++ src/main/java/freshtrash/freshtrashbackend/dto/request/SignUpRequest.java
@@ -1,22 +1,6 @@
 package freshtrash.freshtrashbackend.dto.request;
 
-import freshtrash.freshtrashbackend.entity.Member;
-import freshtrash.freshtrashbackend.entity.constants.AccountStatus;
-import freshtrash.freshtrashbackend.entity.constants.LoginType;
-import freshtrash.freshtrashbackend.entity.constants.UserRole;
-
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
 
-public record SignUpRequest(@NotBlank String nickname, @NotBlank @Email String email, @NotBlank String password) {
-    public Member toEntity() {
-        return Member.builder()
-                .email(email)
-                .password(password)
-                .nickname(nickname)
-                .loginType(LoginType.EMAIL)
-                .userRole(UserRole.USER)
-                .accountStatus(AccountStatus.ACTIVE)
-                .build();
-    }
-}
+public record SignUpRequest(@NotBlank String nickname, @NotBlank @Email String email, @NotBlank String password) {}
