f13dc697aa6426c80141b1c670b94c8ccfb11bd9
garamv20 <garamv20@gmail.com>
2024-04-19 13:14:25 +0900

    refactor: #81-#71반영 rebase 충돌 해결

--- src/test/java/freshtrash/freshtrashbackend/controller/WasteApiTest.java
+++ src/test/java/freshtrash/freshtrashbackend/controller/WasteApiTest.java
@@ -5,12 +5,9 @@ import com.querydsl.core.types.Predicate;
 import freshtrash.freshtrashbackend.Fixture.Fixture;
 import freshtrash.freshtrashbackend.Fixture.FixtureDto;
 import freshtrash.freshtrashbackend.config.TestSecurityConfig;
-<<<<<<< HEAD
-=======
 import freshtrash.freshtrashbackend.dto.response.WasteResponse;
->>>>>>> eb4f5e0 (refactor: #81-Dto -> Response로 통일)
 import freshtrash.freshtrashbackend.dto.request.WasteRequest;
-import freshtrash.freshtrashbackend.dto.response.WasteResponse;
+
 import freshtrash.freshtrashbackend.dto.security.MemberPrincipal;
 import freshtrash.freshtrashbackend.entity.Address;
 import freshtrash.freshtrashbackend.entity.Waste;
