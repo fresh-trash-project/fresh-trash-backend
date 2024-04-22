1ef80dc51888421ab71416fbfd738b5a928c7e73
JadeKim042386 <jk042386@gmail.com>
2024-04-22 13:12:29 +0900

    chore: 프론트와 도메인 포트가 상이하여 CORS 에러가 발생하기 때문에 이와 관련한 설정 추가 - localhost:5173 Origin 설정 - GET, POST, PUT, DELETE Method 허용 설정

---
+++ src/main/java/freshtrash/freshtrashbackend/config/MvcConfig.java
@@ -0,0 +1,19 @@
+package freshtrash.freshtrashbackend.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableScheduling
+@EnableTransactionManagement
+public class MvcConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:5173")
+                .allowedMethods("GET", "POST", "PUT", "DELETE");
+    }
+}
