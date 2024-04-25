dbbc867b907532f91d56464fa4a26d6e6e752829
JadeKim042386 <jk042386@gmail.com>
2024-04-26 00:29:45 +0900

    chore: 프론트에서 Access-Control-Allow-Credential 에러가 발생하는 문제 - MvcConfig에 allowCredentials를 true로 설정하여 해결

--- src/main/java/freshtrash/freshtrashbackend/config/MvcConfig.java
+++ src/main/java/freshtrash/freshtrashbackend/config/MvcConfig.java
@@ -14,6 +14,7 @@ public class MvcConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins("http://localhost:5173")
+                .allowCredentials(true)
                 .allowedMethods("GET", "POST", "PUT", "DELETE");
     }
 }
