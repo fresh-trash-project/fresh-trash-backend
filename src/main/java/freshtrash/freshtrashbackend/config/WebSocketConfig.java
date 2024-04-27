e87dc41627881ac19ca26e59f33706971ea76580
JadeKim042386 <jk042386@gmail.com>
2024-04-27 14:05:52 +0900

    feat: 웹소켓 연결 엔드포인트 및 prefix 수정

--- src/main/java/freshtrash/freshtrashbackend/config/WebSocketConfig.java
+++ src/main/java/freshtrash/freshtrashbackend/config/WebSocketConfig.java
@@ -13,13 +13,13 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         // 클라이언트가 연결할 웹소켓 엔드포인트 설정
-        registry.addEndpoint("/ws").setAllowedOrigins("*");
+        registry.addEndpoint("/chat-ws").setAllowedOrigins("http://localhost:5173");
     }
 
     @Override
     public void configureMessageBroker(MessageBrokerRegistry registry) {
         // 메시지를 라우팅할 경로의 접두사 설정
-        registry.enableSimpleBroker("/topic");
+        registry.enableSimpleBroker("/chat");
         registry.setApplicationDestinationPrefixes("/app");
     }
 }
