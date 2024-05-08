a88e95733fa534f871cc66460ef78847e4fa385b
garamv20 <garamv20@gmail.com>
2024-05-08 15:39:57 +0900

    refactor: #130-고정값 전역변수 추가

--- src/main/java/freshtrash/freshtrashbackend/utils/PasswordGenerator.java
+++ src/main/java/freshtrash/freshtrashbackend/utils/PasswordGenerator.java
@@ -10,9 +10,10 @@ import java.util.List;
 
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class PasswordGenerator {
-    // 영어 소문자, 숫자, 특수문자를 포함한 모든 문자들
     private static final String CHARACTERS = "abcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()-_=+";
-
+    private static final String ALPHABET = "abcdefghijklmnopqrstuvwxyz";
+    private static final String NUMBER = "0123456789";
+    private static final String SPECIAL_CHARACTER = "!@#$%^&*()-_=+";
     private static final int PASSWORD_LENGTH = 8;
 
     public static String generateTemporaryPassword() {
@@ -21,9 +22,9 @@ public class PasswordGenerator {
         SecureRandom random = new SecureRandom();
 
         // 영어 소문자, 숫자, 특수문자를 각각 한 개씩 포함
-        passwordChars.add(getRandomCharacter("abcdefghijklmnopqrstuvwxyz", random));
-        passwordChars.add(getRandomCharacter("0123456789", random));
-        passwordChars.add(getRandomCharacter("!@#$%^&*()-_=+", random));
+        passwordChars.add(getRandomCharacter(ALPHABET, random));
+        passwordChars.add(getRandomCharacter(NUMBER, random));
+        passwordChars.add(getRandomCharacter(SPECIAL_CHARACTER, random));
 
         for (int i = 3; i < PASSWORD_LENGTH; i++) {
             passwordChars.add(CHARACTERS.charAt(random.nextInt(CHARACTERS.length())));
