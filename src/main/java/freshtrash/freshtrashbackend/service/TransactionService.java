a4a350421b8a1abbbf7837b0bc27abf65f0da2b8
garamv20 <garamv20@gmail.com>
2024-05-12 23:10:55 +0900

    style: #130-주석 들여쓰기 된거 기존으로 돌림

--- src/main/java/freshtrash/freshtrashbackend/service/TransactionService.java
+++ src/main/java/freshtrash/freshtrashbackend/service/TransactionService.java
@@ -22,20 +22,20 @@ public class TransactionService {
 
     public Page<WasteResponse> getTransactedWastes(Long memberId, TransactionMemberType memberType, Pageable pageable) {
         switch (memberType) {
-                // 판매 완료 wastes
+            // 판매 완료 wastes
             case SELLER_CLOSE -> {
                 return transactionLogRepository
                         .findAllBySeller_Id(memberId, pageable)
                         .map(TransactionLog::getWaste)
                         .map(WasteResponse::fromEntity);
             }
-                // 판매 중 또는 예약 중 wastes
+            // 판매 중 또는 예약 중 wastes
             case SELLER_ONGOING -> {
                 return wasteRepository
                         .findAllByMemberIdAndSellStatusNot(memberId, SellStatus.CLOSE, pageable)
                         .map(WasteResponse::fromEntity);
             }
-                // 구매 wastes
+            // 구매 wastes
             default -> {
                 return transactionLogRepository
                         .findAllByBuyer_Id(memberId, pageable)
