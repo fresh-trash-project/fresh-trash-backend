00d98e54552ce3826bb784bf08f74af41268b80a
JadeKim042386 <jk042386@gmail.com>
2024-06-07 21:53:07 +0900

    feat: #166 - 경매 입찰 요청 시 10원 단위인지 확인하는 조건 추가 - Retry의 BackOffPolicy에 설정한 delay 변경 - 통합 테스트를 위해 sleep을 거는 코드를 추가

--- src/main/java/freshtrash/freshtrashbackend/exception/constants/ErrorCode.java
+++ src/main/java/freshtrash/freshtrashbackend/exception/constants/ErrorCode.java
@@ -52,7 +52,7 @@ public enum ErrorCode {
     FORBIDDEN_AUCTION(HttpStatus.FORBIDDEN, "경매에 대한 권한이 없습니다."),
     INVALID_AUCTION_TIME(HttpStatus.BAD_REQUEST, "경매 시간이 잘못되었습니다."),
     WRITER_CANT_BIDDING(HttpStatus.BAD_REQUEST, "경매 등록 사용자는 입찰할 수 없습니다."),
-    INVALID_BIDDING_PRICE(HttpStatus.BAD_REQUEST, "요청 입찰가는 기존 입찰가보다 커야합니다."),
+    INVALID_BIDDING_PRICE(HttpStatus.BAD_REQUEST, "요청 입찰가는 기존 입찰가보다 커야하고 최소 10원 단위의 금액을 입력해야합니다."),
     CANT_BIDDING_TIME(HttpStatus.BAD_REQUEST, "지금은 경매 중이 아닙니다.");
 
     private final HttpStatus status;
