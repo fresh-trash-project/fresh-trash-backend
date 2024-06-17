0527193c9d34e294ae8a53b78faf1d79730c91a3
JadeKim042386 <jk042386@gmail.com>
2024-06-17 16:11:06 +0900

    chore: #176 - biddingHistory 테이블 수정

--- database/mariadb/initdb.d/insert_data.sql
+++ database/mariadb/initdb.d/insert_data.sql
@@ -190,7 +190,8 @@ values (1, 'test.png', 'title', 'content', 'CLOTHING', 'GOOD', 'ONGOING', 1000, 
         date_add(now(), interval 1 hour), now()),
        (4, 'test.png', 'title4', 'content4', 'SPORTS', 'GOOD', 'ONGOING', 3000, 5, now(), now(), now());
 
-insert into bidding_history(member_id, auction_id, price, is_success_bidding, created_at)
+insert into bidding_history(member_id, auction_id, price, is_pay, created_at)
 values (2, 1, 1000, true, now()),
        (1, 2, 1000, false, now()),
+       (1, 2, 2000, false, now()),
        (1, 3, 3000, true, now());
