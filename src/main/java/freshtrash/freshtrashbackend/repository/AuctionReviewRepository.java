92c241cce525c241eb5aaa2170daa5296382267b
JadeKim042386 <jk042386@gmail.com>
2024-06-19 08:04:22 +0900

    feat: AuctionReviewRepository에서 Transactional의 propagarion 설정 - 다른 레포지토리와 일관성있게 SUPPORTS 타입으로 설정

--- src/main/java/freshtrash/freshtrashbackend/repository/AuctionReviewRepository.java
+++ src/main/java/freshtrash/freshtrashbackend/repository/AuctionReviewRepository.java
@@ -2,7 +2,10 @@ package freshtrash.freshtrashbackend.repository;
 
 import freshtrash.freshtrashbackend.entity.AuctionReview;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
 
+@Transactional(propagation = Propagation.SUPPORTS)
 public interface AuctionReviewRepository extends JpaRepository<AuctionReview, Long> {
     boolean existsByAuctionId(Long auctionId);
 }
