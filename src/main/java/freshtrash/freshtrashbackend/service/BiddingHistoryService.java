122039cdf6e5ecd3f400d78b90b3597bf1eb1877
JadeKim042386 <jk042386@gmail.com>
2024-06-18 15:41:53 +0900

    refactor: #178 - 결제 완료 처리 후 알람 전송하는 로직에 Template Method 패턴 적용 - biddingHistory의 isPay 속성을 true로 업데이트하고 알림을 전송하는 로직을 template method로 구현

--- src/main/java/freshtrash/freshtrashbackend/service/BiddingHistoryService.java
+++ src/main/java/freshtrash/freshtrashbackend/service/BiddingHistoryService.java
@@ -4,7 +4,7 @@ import freshtrash.freshtrashbackend.entity.BiddingHistory;
 import freshtrash.freshtrashbackend.exception.BiddingHistoryException;
 import freshtrash.freshtrashbackend.exception.constants.ErrorCode;
 import freshtrash.freshtrashbackend.repository.BiddingHistoryRepository;
-import freshtrash.freshtrashbackend.service.producer.AuctionPublisher;
+import freshtrash.freshtrashbackend.service.alarm.CompletePayAuctionAlarm;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -15,7 +15,7 @@ import org.springframework.transaction.annotation.Transactional;
 @RequiredArgsConstructor
 public class BiddingHistoryService {
     private final BiddingHistoryRepository biddingHistoryRepository;
-    private final AuctionPublisher auctionPublisher;
+    private final CompletePayAuctionAlarm completePayAuctionAlarm;
 
     public void addBiddingHistory(Long auctionId, Long memberId, int price) {
         biddingHistoryRepository.save(BiddingHistory.builder()
@@ -29,16 +29,14 @@ public class BiddingHistoryService {
     public void updateToCompletedPayAndNotify(Long auctionId, Long memberId) {
         log.debug("auctionId {} 경매에 입찰한 memberId {} 유저가 입찰한 내역 중 가장 큰 금액으로 입찰한 내역 조회", auctionId, memberId);
         BiddingHistory biddingHistory = getBiddingHistoryByAuctionIdAndMemberId(auctionId, memberId);
-        biddingHistory.setPay(true);
-        log.debug("결제한 유저와 판매자에게 결제 완료 알림 전송");
-        auctionPublisher.publishForCompletedPayAndRequestDelivery(biddingHistory);
+        completePayAuctionAlarm.sendAlarm(biddingHistory);
     }
 
     public void updateSuccessBidAt(Long auctionId) {
         biddingHistoryRepository.updateSuccessBidAtByAuctionId(auctionId);
     }
 
-    private BiddingHistory getBiddingHistoryByAuctionIdAndMemberId(Long auctionId, Long memberId) {
+    public BiddingHistory getBiddingHistoryByAuctionIdAndMemberId(Long auctionId, Long memberId) {
         return biddingHistoryRepository
                 .findFirstByAuctionIdAndMemberIdOrderByPriceDesc(auctionId, memberId)
                 .orElseThrow(() -> new BiddingHistoryException(ErrorCode.NOT_FOUND_BIDDING_HISTORY));
---
+++ src/main/java/freshtrash/freshtrashbackend/service/alarm/CompletePayAuctionAlarm.java
@@ -0,0 +1,28 @@
+package freshtrash.freshtrashbackend.service.alarm;
+
+import freshtrash.freshtrashbackend.entity.BiddingHistory;
+import freshtrash.freshtrashbackend.service.alarm.template.BiddingHistoryAlarmTemplate;
+import freshtrash.freshtrashbackend.service.producer.AuctionPublisher;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+public class CompletePayAuctionAlarm extends BiddingHistoryAlarmTemplate {
+
+    public CompletePayAuctionAlarm(AuctionPublisher producer) {
+        super(producer);
+    }
+
+    @Override
+    public void update(BiddingHistory biddingHistory) {
+        log.debug("결제 여부를 true로 업데이트");
+        biddingHistory.setPay(true);
+    }
+
+    @Override
+    public void publishEvent(BiddingHistory biddingHistory) {
+        log.debug("결제한 유저와 판매자에게 결제 완료 알림 전송");
+        this.producer.publishForCompletedPayAndRequestDelivery(biddingHistory);
+    }
+}
